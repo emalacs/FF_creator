a0b9392ea3219501ec2705a649d87ae712052179
emanuele <emanuele.scalone@gmail.com>
2020-02-14 18:00:14 +0100

    copy warning

--- analysis/analysis.R
+++ analysis/analysis.R
@@ -5,12 +5,40 @@
 
 
 library(tidyverse)
+library(plot3D)
 #sample_file <- here::here("data/sample.csv")
 
-dat <- read.csv("analysis/histo-time.dat", sep = '\t')
+dat <- read.csv("analysis/histo-time.dat", sep = '', header = FALSE)
+
+nmr_molecules <- seq(from = 0, to = (ncol(dat) -1))
+size <- 's'
+#nmr_molecules <- paste0(size, "_", nmr_molecules)
+#nmr_molecules <- paste(nmr_molecules, "_size", sep = "")
+dat_index <- seq(from = 0, to = (ncol(dat) -1))
+
+
+
+
+colnames(dat) <- nmr_molecules
+#nmr_molecules
 dat
+z <- data.matrix(dat)
+#colnames(z) <- NULL
+#rownames(z) <- NULL
+#z
+dat_index[2]=0
+z <- t(z)*dat_index
+z
+#z[,-2]
+#dat_index
+#z <- z[,-20]
+#ncol(z)
+#nrow(z)
+hist3D(z = z, theta = -180)
 
 
-ncol(dat)
-nrow(dat)
+#dat
+
+#ncol(dat)
+#nrow(dat)
 # pesato sulla dimensioni
--- temp.py
+++ temp.py
@@ -1,7 +1,39 @@
 import pandas as pd
+import numpy as np
 
 pep_atoms = pd.read_csv('input/pep_atoms', sep = '\s+', header = None)
 
 dat = pd.read_csv('analysis/histo-time.dat', sep = '\s+')
 
 print(dat.to_string())
+
+x = list(range(0, (len(dat.columns))))
+print(x)
+dat.columns = [x]
+dat.rename(columns = {0:'frame'}, inplace = True)
+print(dat)
+# print(x)
+y = dat[['frame']]
+
+# print(y)
+
+import matplotlib.pyplot as plt
+import numpy as np
+from mpl_toolkits.mplot3d import Axes3D
+
+fig = plt.figure()
+ax = plt.axes(projection = '3d')
+
+num_bars = 3248
+x_pos = len(x)
+y_pos = len(x)
+z_pos = len(x)
+x_size = x
+y_size = y
+z_size = dat[1:]
+
+ax.bar3d(x_pos, y_pos, z_pos, x_size, y_size, z_size, color='aqua')
+plt.show()
+
+
+exit()
