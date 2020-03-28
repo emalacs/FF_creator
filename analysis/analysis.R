11fdf771e9d812d270fc92e0b3fe811b8c4c1f47
Emanuele <emanuele.scalone@gmail.com>
2020-03-28 15:53:29 +0100

    Data analysis new

--- analysis/analysis.R
+++ analysis/analysis.R
@@ -31,60 +31,47 @@ histogram <- cbind(histogram, total = rowSums(is_a_fibril))
 total <- cbind(total = rowSums(is_a_fibril))
 total_to_plot <- melt(total, varnames = c("time", "fibril"))
 
+
+
 # Fibril histogram
 ggplot(data = total_to_plot, aes(x = time, y = value)) + geom_col(aes(fill = value)) +
-  scale_fill_gradient(low = "white", high = "dark blue")
+  scale_fill_gradient(low = "#AC80A0", high = "#0471A6", name = "Fibril MW") +
+  theme_bw() +
+  scale_y_discrete(name = "Fibril MW")
 
-
-# ggplot(data = total_to_plot, aes(x = time, y = value)) + geom_col(aes(fill = value)) +
-#   scale_fill_gradient(low = "white", high = "dark blue") # questo ci piace!!
 
 # Matrix of the entire clustsize to make the heatmap
 matrix_nclust <- melt(nclust, varnames = c("time", "cluster_size"))
 colnames(matrix_nclust)[3] <- "cluster_amount_MW"
-matrix_no0 <- matrix_nclust[matrix_nclust$cluster_amount_MW != 0, ]
+matrix_nclust[matrix_nclust == 0] <- NA
 
 # QUESTA è UN'IDEA MOLTO CARINA MA STO AVENDO UN ATTIMO DI PROBLEMI
-ggplot(data = matrix_no0, aes(x = time, y = cluster_size)) + geom_point(aes(size = cluster_amount_MW,
-                                                                            colour = cluster_amount_MW)) +
-  #SIZE E COLOR PER CLASSI!!
-  #scale_fill_gradient2(low = "white", high = "dark blue") + # Questo va bene
-  scale_y_discrete(breaks = c("size_0", "size_200", "size_300", "size_400", "size_500", "size_600"),
+ggplot(data = matrix_nclust, aes(x = time, y = cluster_size)) + geom_point(aes(size = cluster_amount_MW,
+                                                                               colour = cluster_amount_MW)) +
+  scale_y_discrete(breaks = c("size_100", "size_200", "size_300", "size_400", "size_500", "size_600", "size_700"),
                    name = "Molecules in a Cluster") +
-  scale_x_discrete(name = "Time") +
-  labs(title = "Heat map of fibrils elongation") +
-  theme(axis.title.y = element_text(face = "bold"),
-        axis.text.y = element_text(size = 7)) +
   scale_size(guide = "none") +
-  expand_limits(x = 2, y = 50)
+  scale_colour_gradient(low = "#AC80A0", high = "#0471A6", na.value = "white", name = "Amount") +
+  theme_bw() +
+  expand_limits(y = 700)
+
 
 # QUESTO CI PIACE, AL MOMENTO LO LASCIO COSì. SI PUò TOGLIERE IL COLORE IN BASE ALLA DIMENSIONE PERCHé TANTO è 2D
-ggplot(data = matrix_no0, aes(x = time, y = cluster_size)) + geom_raster(aes(fill = cluster_amount_MW)) + # ok
-  #scale_fill_gradient2(low = "white", high = "dark blue", mid = "light blue", midpoint = 0.8) +
-  scale_fill_gradient(low = "white", high = "dark blue") + # Questo va bene
+ggplot(data = matrix_nclust, aes(x = time, y = cluster_size)) + geom_raster(aes(fill = cluster_amount_MW)) + # ok
+  scale_fill_gradient(low = "#AC80A0", high = "#0471A6", na.value = "white") + # Questo va bene
   scale_y_discrete(breaks = c("size_100", "size_200", "size_300", "size_400", "size_500", "size_600"),
                    name = "Molecules in a Cluster") +
-  #scale_x_discrete(name = "Time") +
   labs(title = "Heat map of fibrils elongation", fill = "Amount") +
-  theme(axis.title.y = element_text(face = "bold"),
-        axis.text.y = element_text(size = 7))
+  theme_bw()
 
 
-#library(ggplot2)
-#ggplot(mtcars, aes(wt, mpg)) +
-#  geom_point(aes(colour = cut(qsec, c(-Inf, 17, 19, Inf))),
-#             size = 5) +
-#  scale_color_manual(name = "qsec",
-#                     values = c("(-Inf,17]" = "black",
-#                                  "(17,19]" = "yellow",
-#                                  "(19, Inf]" = "red"),
-#                     labels = c("<= 17", "17 < qsec <= 19", "> 19"))
-
-
-
-
-#ggplot(data = matrix_no0, aes(x = time, y = cluster_size)) + geom_point(aes(size = cluster_amount_MW,
-#                                                                            colour = cut(cluster_amount_MW, c (100, 200)))) +
-#scale_color_manual(name = "cluster_amount_MW",
-#                    values = c("(100,200]" = "yellow"),
-#                    labels = c("<=100", "17 <x<19", "<19")) +
+# MERGE
+ggplot(data = matrix_nclust, aes(x = time, y = cluster_size)) + geom_raster(aes(fill = cluster_amount_MW)) + # ok
+  scale_fill_gradient(low = "#AC80A0", high = "#0471A6", na.value = "white") + # Questo va bene
+  labs(title = "Heat map of fibrils elongation", fill = "Amount") +
+  geom_point(aes(size = cluster_amount_MW, colour = cluster_amount_MW), alpha = 0.5) +
+  scale_size(guide = "none") +
+  scale_y_discrete(breaks = c("size_100", "size_200", "size_300", "size_400", "size_500", "size_600"),
+                   name = "Molecules in a Cluster") +
+  scale_colour_gradient(low = "#AC80A0", high = "#0471A6", na.value = "white", name = "Amount") +
+  theme_bw()
