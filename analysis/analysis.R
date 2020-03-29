d524d254a00bbc736fbf0f5e098b0819fb92b138
Emanuele <emanuele.scalone@gmail.com>
2020-03-29 18:43:53 +0200

    Data analysis new

--- analysis/analysis.R
+++ analysis/analysis.R
@@ -47,31 +47,34 @@ matrix_nclust[matrix_nclust == 0] <- NA
 
 # QUESTA è UN'IDEA MOLTO CARINA MA STO AVENDO UN ATTIMO DI PROBLEMI
 ggplot(data = matrix_nclust, aes(x = time, y = cluster_size)) + geom_point(aes(size = cluster_amount_MW,
-                                                                               colour = cluster_amount_MW)) +
+                                                                               colour = cluster_amount_MW),
+                                                                           alpha = 1/50) +
   scale_y_discrete(breaks = c("size_100", "size_200", "size_300", "size_400", "size_500", "size_600", "size_700"),
                    name = "Molecules in a Cluster") +
-  scale_size(guide = "none") +
-  scale_colour_gradient(low = "#AC80A0", high = "#0471A6", na.value = "white", name = "Amount") +
+  scale_radius(guide = "none") +
+  scale_colour_gradient(low = "#AC80A0", high = "#0471A6", na.value = "white", name = "Amount", guide = "none") +
   theme_bw() +
   expand_limits(y = 700)
 
 
 # QUESTO CI PIACE, AL MOMENTO LO LASCIO COSì. SI PUò TOGLIERE IL COLORE IN BASE ALLA DIMENSIONE PERCHé TANTO è 2D
 ggplot(data = matrix_nclust, aes(x = time, y = cluster_size)) + geom_raster(aes(fill = cluster_amount_MW)) + # ok
-  scale_fill_gradient(low = "#AC80A0", high = "#0471A6", na.value = "white") + # Questo va bene
+  scale_fill_gradient(low = "#AC80A0", high = "#0471A6", na.value = "white", guide = "none") + # Questo va bene
   scale_y_discrete(breaks = c("size_100", "size_200", "size_300", "size_400", "size_500", "size_600"),
                    name = "Molecules in a Cluster") +
   labs(title = "Heat map of fibrils elongation", fill = "Amount") +
+  expand_limits(y = 700) +
   theme_bw()
 
 
 # MERGE
 ggplot(data = matrix_nclust, aes(x = time, y = cluster_size)) + geom_raster(aes(fill = cluster_amount_MW)) + # ok
-  scale_fill_gradient(low = "#AC80A0", high = "#0471A6", na.value = "white") + # Questo va bene
-  labs(title = "Heat map of fibrils elongation", fill = "Amount") +
-  geom_point(aes(size = cluster_amount_MW, colour = cluster_amount_MW), alpha = 0.5) +
-  scale_size(guide = "none") +
+  scale_fill_gradient(low = "#AC80A0", high = "#0471A6", na.value = "white", guide = "none") + # Questo va bene
+  labs(title = "Heat map of fibrils elongation") +
+  geom_point(aes(size = cluster_amount_MW, colour = cluster_amount_MW), alpha = 1/40) +
+  scale_radius(guide = "none") +
   scale_y_discrete(breaks = c("size_100", "size_200", "size_300", "size_400", "size_500", "size_600"),
                    name = "Molecules in a Cluster") +
-  scale_colour_gradient(low = "#AC80A0", high = "#0471A6", na.value = "white", name = "Amount") +
+  scale_colour_gradient(low = "#AC80A0", high = "#0471A6", na.value = "white", guide = "none") +
+  expand_limits(y = 700) +
   theme_bw()
